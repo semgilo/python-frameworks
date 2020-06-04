65a8fa0878ac2b5f67f9e0df1d3b437cceea6108
semgilo <9737935@qq.com>
2020-06-04 16:22:43 +0800

    xxtea

--- utils/crypto.py
+++ utils/crypto.py
@@ -1,32 +1,10 @@
 import struct
 import sys
+import xxtea
 ACSII_A = 65
 ACSII_Z = 90
 ACSII_a = 97
 ACSII_z = 122
-
-_DELTA = 0x9E3779B9
-
-
-def _long2str(v, w):
-    n = (len(v) - 1) << 2
-    if w:
-        m = v[-1]
-        if (m < n - 3) or (m > n):
-            return ''
-        n = m
-    s = struct.pack('<%iL' % len(v), *v)
-    return s[0:n]
-
-
-def _str2long(s, w):
-    n = len(s)
-    m = (4 - (n & 3) & 3) + n
-    s = s.ljust(m, "\0")
-    v = list(struct.unpack('<%iL' % (m >> 2), s))
-    if w:
-        v.append(n)
-    return v
 
 
 class Crypto:
@@ -36,63 +14,29 @@ class Crypto:
         super(Crypto, self).__init__()
 
     @staticmethod
-    def encrypt_by_xxtea(str, key):
-        if str == '':
-            return str
-        v = _str2long(str, True)
-        k = _str2long(key.ljust(16, "\0"), False)
-        n = len(v) - 1
-        z = v[n]
-        y = v[0]
-        sum = 0
-        q = 6 + 52 // (n + 1)
-        while q > 0:
-            sum = (sum + _DELTA) & 0xffffffff
-            e = sum >> 2 & 3
-            for p in range(n):
-                y = v[p + 1]
-                v[p] = (v[p] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
-                                ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff
-                z = v[p]
-            y = v[0]
-            v[n] = (v[n] + ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
-                            ^ (sum ^ y) + (k[n & 3 ^ e] ^ z))) & 0xffffffff
-            z = v[n]
-            q -= 1
-        return _long2str(v, False)
-
-    @staticmethod
-    def decrypt_by_xxtea(str, key):
-        if str == '':
-            return str
-        v = _str2long(str, False)
-        k = _str2long(key.ljust(16, "\0"), False)
-        n = len(v) - 1
-        z = v[n]
-        y = v[0]
-        q = 6 + 52 // (n + 1)
-        sum = (q * _DELTA) & 0xffffffff
-        while (sum != 0):
-            e = sum >> 2 & 3
-            for p in range(n, 0, -1):
-                z = v[p - 1]
-                v[p] = (v[p] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
-                                ^ (sum ^ y) + (k[p & 3 ^ e] ^ z))) & 0xffffffff
-                y = v[p]
-            z = v[n]
-            v[0] = (v[0] - ((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)
-                            ^ (sum ^ y) + (k[0 & 3 ^ e] ^ z))) & 0xffffffff
-            y = v[0]
-            sum = (sum - _DELTA) & 0xffffffff
-        return _long2str(v, True)
-
-    @staticmethod
     def have_encrypt(path, sign):
         bytesFile = open(path, "rb+")
         buff = bytesFile.read()
         pre_sign = str(buff[0:len(sign)])
         bytesFile.close()
         return pre_sign == sign
+
+    @staticmethod
+    def encrypt_file(path, key, sign):
+        bytesFile = open(path, "rb+")
+
+        buff = bytesFile.read()
+        pre_sign = str(buff[0:len(sign)])
+        if pre_sign == sign:
+            print("have entrypt :" + path)
+        else:
+            print("entrypt file :" + path)
+            encryBytes = xxtea.encrypt(buff, key)
+            encryBytes = sign.encode("utf-8") + encryBytes
+            bytesFile.seek(0)
+            bytesFile.write(encryBytes)
+
+        bytesFile.close()
 
     @staticmethod
     def encrypt_by_ascii(content, encode_key):
